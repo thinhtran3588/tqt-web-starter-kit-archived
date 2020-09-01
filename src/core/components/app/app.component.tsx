7086bfffa3f8924a81067ae3c3cb105c88c1e9d1
Thinh Tran <quangthinhtran3588@gmail.com>
2020-09-01 12:58:49 +0700

    add css baseline

--- src/core/components/app/app.component.tsx
+++ src/core/components/app/app.component.tsx
@@ -4,6 +4,7 @@ import type {AppProps} from 'next/app';
 import Head from 'next/head';
 import {I18nProvider} from 'next-localization';
 import {ThemeProvider} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {theme} from '@app/core/theme';
 
 export const App = ({Component, pageProps}: AppProps): JSX.Element => {
@@ -15,6 +16,7 @@ export const App = ({Component, pageProps}: AppProps): JSX.Element => {
           content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover'
         />
       </Head>
+      <CssBaseline />
       <ThemeProvider theme={theme}>
         <I18nProvider lngDict={pageProps.lngDict} locale={pageProps.lng}>
           <Component {...pageProps} />
