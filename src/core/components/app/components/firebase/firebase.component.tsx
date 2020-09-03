97d4733f33f934c519eebb0383436771bd5c275f
Thinh Tran <quangthinhtran3588@gmail.com>
2020-09-04 06:43:59 +0700

    fix env var

--- src/core/components/app/components/firebase/firebase.component.tsx
+++ src/core/components/app/components/firebase/firebase.component.tsx
@@ -23,7 +23,7 @@ export const Firebase = (): JSX.Element => {
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
       messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-      measurementId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     };
 
     if (!firebase.apps || firebase.apps.length === 0) {
